ca6c227dc6ad5880502e04839bb69cb95a11faed
tcyang <tcyang@gm.pu.edu.tw>
2021-03-26 15:49:20 +0800

    圖片長按-1

--- app/src/main/java/tw/edu/pu/csim/tcyang/counter20210319/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/counter20210319/MainActivity.kt
@@ -5,7 +5,8 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(),
+    View.OnClickListener, View.OnLongClickListener {
 
     var counter:Int = 0
 
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         txv.text = counter.toString()
 
         txv.setOnClickListener(this)
+        txv.setOnLongClickListener(this)
         btn.setOnClickListener(this)
         btn2.setOnClickListener(this)
         btn3.setOnClickListener(this)
@@ -26,6 +28,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
 
         })
+
+        img.setOnLongClickListener(this)
     }
 
 
@@ -41,6 +45,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             counter=0
         }
         txv.text = counter.toString()
+    }
+
+    override fun onLongClick(v: View?): Boolean {
+        if (v==txv){
+            counter += 2
+        }
+        else{
+            counter--
+        }
+
+        txv.text = counter.toString()
+        return true
     }
 
 
